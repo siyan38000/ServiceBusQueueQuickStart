c4c2534c1b03f5da96163ebde04a0a3cffdfa5b2
Siyan <github@siyan.fr>
2022-02-06 21:14:57 +0100

    Ajout des models

---
+++ QueueSender/Models/Camtar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueSender.Models
+{
+    public class Camtar
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Boolean IsArrived { get; set; }  
+
+        public Boolean  IsLeft { get; set; }
+    }
+}
