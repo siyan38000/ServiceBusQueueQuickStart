b3c2126a7851f0ca60913381730ca21d8f9469e7
Siyan <github@siyan.fr>
2022-02-22 00:00:03 +0100

    Youpi, ca envoi des msg random dans la grosse queue

--- QueueSender/Program.cs
+++ QueueSender/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Text.Json;
+using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
+using QueueSender.Models;
+
 public class Program
 {
     // connection string to your Service Bus namespace
@@ -17,7 +20,7 @@ public class Program
     // number of messages to be sent to the queue
     private const int numOfMessages = 3;
 
-    static async Task Main()
+    static async Task SendMessages(string message)
     {
         // The Service Bus client types are safe to cache and use as a singleton for the lifetime
         // of the application, which is best practice when messages are being published or read
@@ -30,21 +33,18 @@ public class Program
         // create a batch 
         using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
 
-        for (int i = 1; i <= numOfMessages; i++)
-        {
             // try adding a message to the batch
-            if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+            if (!messageBatch.TryAddMessage(new ServiceBusMessage(message)))
             {
                 // if it is too large for the batch
-                throw new Exception($"The message {i} is too large to fit in the batch.");
+                throw new Exception($"The message is too large to fit in the batch.");
             }
-        }
 
         try
         {
             // Use the producer client to send the batch of messages to the Service Bus queue
             await sender.SendMessagesAsync(messageBatch);
-            Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            Console.WriteLine($"message envoyé : "+message);
         }
         finally
         {
@@ -53,8 +53,59 @@ public class Program
             await sender.DisposeAsync();
             await client.DisposeAsync();
         }
+    }
+    public static void Main()
+    {
+        var startTimeSpan = TimeSpan.Zero;
+        var periodTimeSpan = TimeSpan.FromSeconds(30);
 
-        Console.WriteLine("Press any key to end the application");
+        var timer = new Timer((e) =>
+        {
+            string messageToSend = GenerateRandomMessage();
+            SendMessages(messageToSend);
+        }, null, startTimeSpan, periodTimeSpan);
         Console.ReadKey();
+    }
+    public static string GenerateRandomMessage()
+    { 
+        Random random = new Random();
+
+        Camtar camion1 = new Camtar();
+        camion1.Name = "Renault Trafic";
+        camion1.Id = 1;
+        camion1.IsLeft = true;
+        camion1.IsArrived = false;
+        camion1.date = DateTime.Now;
+
+        Camtar camion2 = new Camtar();
+        camion2.Name = "Iveco Daily";
+        camion2.Id = 2;
+        camion2.IsLeft = true;
+        camion2.IsArrived = false;
+        camion2.date = DateTime.Now;
+
+        Camtar camion3 = new Camtar();
+        camion3.Name = "Mercedes-Benz Sprinter";
+        camion3.Id = 3;
+        camion3.IsLeft = false;
+        camion3.IsArrived = true;
+        camion3.date = DateTime.Now;
+
+        Camtar camion4 = new Camtar();
+        camion4.Name = "Renault Master";
+        camion4.Id = 4;
+        camion4.IsLeft = false;
+        camion4.IsArrived = true;
+        camion4.date = DateTime.Now;
+
+        Camtar[] camions = new Camtar[4];
+        camions[0] = camion1;
+        camions[1] = camion2;
+        camions[2] = camion3;
+        camions[3] = camion4;
+
+        int rndCamtar = random.Next(camions.Length);
+        string message = JsonSerializer.Serialize(camions[rndCamtar]);
+        return message;
     }
 }
