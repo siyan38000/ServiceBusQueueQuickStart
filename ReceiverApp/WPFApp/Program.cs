ad274d3439c36232f6fea84919a60beec75fe516
winsocute <thithaouyen.pham@epsi.fr>
2022-02-24 11:34:39 +0100

    reveiver-app

---
+++ ReceiverApp/WPFApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+// connection string to your Service Bus namespace
+string connectionString = "Endpoint=sb://projet-archi.servicebus.windows.net/;SharedAccessKeyName=Test;SharedAccessKey=2R/kIwrJ+1QnBHMLx11GiFN77vHJ95tyPXx5S+T2FzY=;EntityPath=main-queue";
+
+// name of your Service Bus queue
+string queueName = "main-queue";
+
+// the client that owns the connection and can be used to create senders and receivers
+ServiceBusClient client;
+
+// the processor that reads and processes messages from the queue
+ServiceBusProcessor processor;
+
+// handle received messages
+static async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+
+    // complete the message. messages is deleted from the queue. 
+    await args.CompleteMessageAsync(args.Message);
+}
+
+// handle any errors when receiving messages
+static Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+// Create the client object that will be used to create sender and receiver objects
+client = new ServiceBusClient(connectionString);
+
+// create a processor that we can use to process the messages
+processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+try
+{
+    // add handler to process messages
+    processor.ProcessMessageAsync += MessageHandler;
+
+    // add handler to process any errors
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    // start processing 
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    Console.ReadKey();
+
+    // stop processing 
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+finally
+{
+    // Calling DisposeAsync on client types is required to ensure that network
+    // resources and other unmanaged objects are properly cleaned up.
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
